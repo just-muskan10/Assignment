078e7c7aa31ba1b17236c8283fd838bb60f3bc74
just-muskan10 <muskanrafik786@gmail.com>
2025-06-03 13:42:33 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,19 @@
+# module1.py
+
+a = 10
+print(type(a),a) #Decimal
+
+b = 0b10101
+print(type(b),b) #Binary
+
+c = 0o23014
+print(type(c),c) #octal
+
+d = 0x3ab
+print(type(d),d) #Hexadecimal
+
+b = True
+
+print(type(b))
+
+print(b+b)
